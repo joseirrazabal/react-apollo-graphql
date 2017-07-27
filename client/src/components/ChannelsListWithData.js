b608225a1ea9a8a9c0bb06440e5071248447cf75
jirrazabal <joseirrazabal@aleani.com.ar>
2017-07-27 00:07:25 -0300

    cambios

---
+++ client/src/components/ChannelsListWithData.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { gql, graphql } from 'react-apollo';
+
+import AddChannel from './AddChannel';
+
+const channelsSubscription = gql`
+  subscription channelAdded {
+    channelAdded {
+      id
+      name
+    }
+  }
+`
+
+class ChannelsList extends Component {
+  componentWillMount() {
+    this.props.data.subscribeToMore({
+      document: channelsSubscription,
+      updateQuery: (prev, {subscriptionData}) => {
+        if (!subscriptionData.data) {
+          return prev;
+        }
+
+        const newChannel = subscriptionData.data.channelAdded;
+
+        if (!prev.channels.find((msg) => msg.id === newChannel.id)) {
+          return Object.assign({}, prev, {
+            channels: [...prev.channels, newChannel]
+          });
+        } else {
+          return prev;
+        }
+      }
+    })
+  }
+
+  render() {
+    const { data: {loading, error, channels } } = this.props;
+
+    if (loading) {
+      return <p>Loading ...</p>;
+    }
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    return (
+      <div className="channelsList">
+        <AddChannel />
+        { channels.map( ch =>
+          (<div key={ch.id} className={'channel ' + (ch.id < 0 ? 'optimistic' : '')}>
+            <Link to={ch.id < 0 ? `/` : `channel/${ch.id}`}>
+              {ch.name}
+            </Link>
+          </div>)
+        )}
+      </div>
+    );
+  }
+}
+
+export const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+
+export default (graphql(channelsListQuery, {
+  options: (props) => ({}),
+})(ChannelsList));
---
+++ server/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import {
+  graphqlExpress,
+  graphiqlExpress,
+} from 'graphql-server-express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import { schema } from './src/schema';
+
+import { execute, subscribe } from 'graphql';
+import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost:27017/graph', { useMongoClient: true })
+
+const PORT = 4000;
+const server = express();
+
+server.use('*', cors({ origin: 'http://localhost:3000' }));
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
+
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+  subscriptionsEndpoint: `ws://localhost:4000/subscriptions`
+}));
+
+// We wrap the express server so that we can attach the WebSocket for subscriptions
+const ws = createServer(server);
+
+ws.listen(PORT, () => {
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}`);
+
+  // Set up the WebSocket for handling GraphQL subscriptions
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema
+  }, {
+    server: ws,
+    path: '/subscriptions',
+  });
+});
